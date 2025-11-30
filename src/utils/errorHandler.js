ab13df054612ada4b0f95f0e6142df57f22a70c6
Gustavo Andrade <gugufonceca.2004@gmail.com>
2025-11-30 18:43:41 -0300

    pronto

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -1,6 +1,5 @@
 exports.errorHandler = (err, req, res, next) => {
   console.error(err);
-  // Em produção, não vaze stacktrace
   const message = process.env.NODE_ENV === 'production' ? 'Internal server error' : err.message;
   res.status(500).json({ error: message });
 };
